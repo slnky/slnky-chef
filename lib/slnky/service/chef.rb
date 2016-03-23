58cc85ac805f0e7bb3f23695cae75dfc67b06e4f
Shawn Catanzarite <me@shawncatz.com>
2016-03-23 02:49:10 -0700

    error log chef run failures, which should be picked up by hipchat service

--- lib/slnky/service/chef.rb
+++ lib/slnky/service/chef.rb
@@ -14,15 +14,18 @@ module Slnky
       end
 
       subscribe 'aws.ec2.terminated', :handle_terminated
+      subscribe 'chef.run.*', :handle_chef
 
       def handle_terminated(name, data)
         id = data.detail['instance-id']
         remove_instance(id)
       end
 
-      def handler(name, data)
-        # for example test
-        name == 'slnky.service.test' && data.hello == 'world!'
+      def handle_chef(name, data)
+        if name == 'chef.run.failure'
+          name = data.attributes ? "#{data.attributes.name}-#{data.attributes.cluster}" : ""
+          log :error, "chef run failed on #{data.node} #{name}"
+        end
       end
 
       def ridley
