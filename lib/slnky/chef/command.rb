381cdb64ee846e90f824d281f48433f718733a90
Shawn Catanzarite <me@shawncatz.com>
2016-03-30 12:19:04 -0700

    use new docopt-based options

--- lib/slnky/chef/command.rb
+++ lib/slnky/chef/command.rb
@@ -6,20 +6,38 @@ module Slnky
         @client ||= Slnky::Chef::Client.new(config)
       end
 
-      def echo(request, response)
-        opts = options(request.args) do |slop|
-          slop.banner = "usage: echo [options] ..."
-          slop.on "-h", "--help", "print help" do
-            return response.output slop.to_s
-          end
-          slop.int "-x", "--times", "print x times", default: 1
-        end
-        args = opts.args
-        1.upto(opts[:times]) do |i|
+      command :echo, "respond with the given arguments", <<-USAGE.strip_heredoc
+        Usage: echo [options] ARGS...
+
+        -h --help           print help.
+        -x --times=TIMES    print x times [default: 1].
+      USAGE
+
+      command :remove, "remove node and client matching NAME", <<-USAGE.strip_heredoc
+        Usage: remove [options] NAME
+
+        -h --help           print help.
+      USAGE
+
+      def handle_echo(request, response, opts)
+        args = opts.ARGS
+        1.upto(opts.times.to_i) do |i|
           response.output args.join(" ")
         end
       rescue => e
-        response.error "error: #{e.message} at #{e.backtrace.first}"
+        response.error "#{__method__}: #{e.message} at #{e.backtrace.first}"
+      end
+
+      def handle_remove(request, response, opts)
+        name = opts.NAME
+        response.output name
+      rescue => e
+        response.error "#{__method__}: #{e.message} at #{e.backtrace.first}"
+      end
+
+      def help(request, response)
+        response.output '  echo     just respond with the arguments given'
+        response.output '  remove   remove node and client matching NAME'
       end
     end
   end
