d2cc768279c18a15dab5112188fedbaf6768d4b5
Shawn Catanzarite <me@shawncatz.com>
2016-04-14 17:27:56 -0700

    response trace vs the log object, this should resolve it. leaving this pointing at master for now

--- spec/slnky/chef/command_spec.rb
+++ spec/slnky/chef/command_spec.rb
@@ -15,16 +15,16 @@ describe Slnky::Chef::Command do
 
   it 'handles echo' do
     expect { subject.handle(echo.name, echo.payload, echo_response) }.to_not raise_error
-    expect(echo_response.log).to include("test echo")
+    expect(echo_response.trace).to include("test echo")
   end
 
   it 'handles help' do
     expect { subject.handle(help.name, help.payload, help_response) }.to_not raise_error
-    expect(help_response.log).to include("chef help: print help")
+    expect(help_response.trace).to include("chef help: print help")
   end
 
   it 'handles remove' do
     expect { subject.handle(remove.name, remove.payload, remove_response) }.to_not raise_error
-    expect(remove_response.log).to include("i-12345678")
+    expect(remove_response.trace).to include("i-12345678")
   end
 end
